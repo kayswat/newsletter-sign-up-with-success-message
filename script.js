ae474754a2b6eb3dda843a3170dd952f9c6e84c3
Kayswat <kazeemtoheeb@gmail.com>
2023-11-14 10:09:28 +0100

    responsiveness

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const errorMessage = document.querySelector(".error-message");
 const successMessage = document.querySelector(".success-message");
 const hero = document.querySelector(".hero");
 const dismissMessage = document.querySelector("#dismiss-msg");
-
+const attribution = document.querySelector(".attribution");
 
 function validateEmail(e){
     e.preventDefault();
@@ -19,13 +19,13 @@ email.style.color = "red";
 if (!emailValue.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
 errorMessage.style.display = "block";
 email.style.border = "1px solid red";
-email.style.background = "#f7bcbc";
-email.style.color = "red";
+emailValue.style.background = "#f7bcbc";
+emailValue.style.color = "red";
 }
 else{
 successMessage.classList.add("open-success-message");
-hero.classList.add("hide-hero");
-
+ hero.style.display = "none";
+attribution.style.visibility = 'hidden';
 }
 };
 
